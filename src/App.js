4bd8216d7c08641a4a4c703a4df29beed0b99114
Nikita Kholin <nik.kholin@gmail.com>
2017-08-27 23:21:33 +0300

    Add username input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Repositories from './screens/Repositories/';
+import Owner from './screens/Owner/';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: null
+    }
+
+    this.updateUsername = this.updateUsername.bind(this);
+  }
+
+  updateUsername(username) {
+    this.setState({username: username})
+  }
+
   render() {
+    if (this.state.username) return <Repositories username={this.state.username} />
+
     return (
       <div className="App">
         <div className="App-header">
           <h3>Welcome to MiniGithub</h3>
         </div>
-        <Repositories username="hmlon" />
+        <Owner username="hmlon" handleSubmit={this.updateUsername}/>
       </div>
     );
   }
